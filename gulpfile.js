2ec7f4c6391243e37ed4138046bd9b85cfc033ef
Renan Muniz <renan@renanmuniz.com>
2016-06-03 00:30:13 -0300

    Update gulpfile, sass and npm

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,53 @@
 const gulp = require('gulp');
-function magicUpperCaseConvert() {
-	// ...
-}
+const concat = require('gulp-concat');
+const sass = require('gulp-sass');
 
-gulp.task('upper', () => {
-	gulp.src('*.txt')
-	.pipe(magicUpperCaseConvert())
-	.pipe(gulp.dest('upper_files'));
+const child = require('child_process');
+const gutil = require('gulp-util');
+
+const browserSync = require('browser-sync').create();
+const siteRoot = '_site';
+
+const cssFiles = '_sass/**/*.?(s)css';
+
+gulp.task('css', () => {
+	gulp.src(cssFiles)
+	.pipe(sass())
+	.pipe(concat('style.css'))
+	.pipe(gulp.dest('assets'))
 });
 
-gulp.task('watch', () => {
-	gulp.watch('*.txt', ['upper']);
+gulp.task('jekyll', () => {
+	const jekyll = child.spawn('bundle', ['exec', 
+	'jekyll',
+	'build',
+	'--watch',
+	'--incremental',
+	'--drafts'
+	]);
+
+	const jekyllLogger = (buffer) => {
+		buffer.toString()
+		.split(/\n/)
+		.forEach((message) => gutil.log('jekyll: ' + message));
+
+		jekyll.stdout.on('data', jekyllLogger);
+		jekyll.stderr.on('data', jekyllLogger);
+	};
 });
 
-gulp.task('default', ['watch']);
+gulp.task('serve', () => {
+	browserSync.init({
+		files: [siteRoot + '/**'],
+		port: 4000,
+		server: {
+			baseDir: siteRoot
+		}
+	});
+
+	gulp.watch(cssFiles, ['css']);
+});
+
+gulp.task('default', ['css', 'jekyll', 'serve']);
 
 
