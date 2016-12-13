5b05a2c0112e52d28ef19e032ad5d27cc0177df1
Renan Muniz <renan@renanmuniz.com>
2016-12-13 06:54:58 -0200

    Add service-worker and update manifest

--- gulpfile.js
+++ gulpfile.js
@@ -60,4 +60,12 @@ gulp.task('serve', () => {
 
 gulp.task('default', ['css', 'jekyll', 'serve']);
 
+gulp.task('generate-service-worker', function(callback) {
+  var path = require('path');
+  var swPrecache = require('sw-precache');
 
+  swPrecache.write(path.join(siteRoot, 'service-worker.js'), {
+    staticFileGlobs: [siteRoot + '/**/*.{js,html,css,png,jpg,gif,svg,eot,ttf,woff}'],
+    stripPrefix: siteRoot
+  }, callback);
+});
