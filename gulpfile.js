a2ff0e45768666897b99eb8b06b6c7f2e8fbd6ae
Renan Muniz <renan@renanmuniz.com>
2016-12-13 12:05:38 -0200

    Update service-worker path

--- gulpfile.js
+++ gulpfile.js
@@ -64,7 +64,7 @@ gulp.task('generate-service-worker', function(callback) {
   var path = require('path');
   var swPrecache = require('sw-precache');
 
-  swPrecache.write(path.join(siteRoot, 'service-worker.js'), {
+  swPrecache.write('service-worker.js', {
     staticFileGlobs: [siteRoot + '/**/*.{js,html,css,png,jpg,gif,svg,eot,ttf,woff}'],
     stripPrefix: siteRoot
   }, callback);
